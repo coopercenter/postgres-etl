f262a21c5756fa935611acb5b973f431829a8a07
Jackson Brandberg <jacksonbrandberg@gmail.com>
2020-05-26 16:29:41 -0400

    add code to clean rooftop solar

---
+++ cleaning_data_code_jackson/cleaning_rooftop_solar_low_income.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(tidyverse)
+library(stringr) # for replacing strings
+library(here)
+library("RPostgreSQL")
+library(readxl)
+
+db_driver = dbDriver("PostgreSQL")
+source(here('my_postgres_credentials.R'))
+db <- dbConnect(db_driver,user=db_user, password=ra_pwd,dbname="postgres", host=db_host)
+rm(ra_pwd)
+#read in dataset
+rooftop_solar_low_income <- read.csv(here('data','raw_data','rooftop_solar_low_income.csv'))
+rooftop_solar_low_income %>% filter(state_name =='Virginia') ->rooftop_solar_low_income
+
+upload <- function(df){
+  df_name <- deparse(substitute(df))
+  dbWriteTable(db, df_name, df, row.names=FALSE, overwrite = TRUE)
+}
+upload(rooftop_solar_low_income)
