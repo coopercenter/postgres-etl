af487df22865d5df3bd89e98c0de10f43aa54029
Chloe Fauvel <cff2aa@virginia.edu>
2020-05-28 11:27:36 -0400

    minor edits

--- api_data_code/metadata_code_offshore_wind.R
+++ api_data_code/metadata_code_offshore_wind.R
@@ -3,7 +3,7 @@
 library(here)
 library('RPostgreSQL')
 library(tidyverse)
-source(here("etl", "my_postgres_credentials.R"))
+source(here("api_data_code", "my_postgres_credentials.R"))
 db_driver <- dbDriver("PostgreSQL")
 
 db <- dbConnect(db_driver,user=db_user, password=ra_pwd,dbname="postgres", host=db_host)
@@ -14,13 +14,14 @@ rm(ra_pwd)
 dbExistsTable(db, "metadata")
 metadata <- dbGetQuery(db,'SELECT * from metadata')
 # TRUE
+
+# ----------------------------------------------------------------------------------
 total_mw_offshore_wind<-dbGetQuery(db,'SELECT * from total_mw_offshore_wind')
 
 colnames(total_mw_offshore_wind)
 offshore_mw_cols <- list(c('Year','Total','CVOW_Pilot','CVOW_Commercial_Stage_I','CVOW_Commercial_Stage_II',
                     'CVOW_Commercial_Stage_III'))
-offshore_mw_units <-list(c('Year','megawatts','megawatts','megawatts',
-                    'megawatts','megawatts'))
+offshore_mw_units <-'megawatts'
 
 r1<- data.frame(db_table_name = "total_mw_offshore_wind",
                 full_series_name = 'Total megawatt predictions of offshore wind energy in phases from 2017 through 2033',
@@ -29,14 +30,13 @@ r1<- data.frame(db_table_name = "total_mw_offshore_wind",
                 url='https://www.dominionenergy.com/library/domcom/media/about-us/making-energy/2020-va-integrated-resource-plan.pdf?modified=20200501191108',api=NA,
                 series_id=NA,json=NA,notes=NA)
 
-
+# ----------------------------------------------------------------------------------
 net_capacity_factor_offshore_wind<-dbGetQuery(db,'SELECT * from net_capacity_factor_offshore_wind')
 
 colnames(net_capacity_factor_offshore_wind)
 offshore_cf_cols <- list(c('Year','Pilot','Stage_I','Stage_II',
                            'Stage_III'))
-offshore_cf_units <-list(c('Year','CF','CF','CF',
-                           'CF'))
+offshore_cf_units <- 'CF'
 
 r2<- data.frame(db_table_name = "net_capacity_factor_offshore_wind",
                 full_series_name = 'Capacity factor predictions of offshore wind energy in phases from 2017 through 2035',
@@ -45,11 +45,12 @@ r2<- data.frame(db_table_name = "net_capacity_factor_offshore_wind",
                 url='https://www.dominionenergy.com/library/domcom/media/about-us/making-energy/2020-va-integrated-resource-plan.pdf?modified=20200501191108',api=NA,
                 series_id=NA,json=NA,notes=NA)
 
+# ----------------------------------------------------------------------------------
 total_production_forecast_offshore_wind<-dbGetQuery(db,'SELECT * from total_production_forecast_offshore_wind')
 colnames(total_production_forecast_offshore_wind)
 offshore_tp_cols <- list(c('Year','Total_Production_GWh'))
-offshore_tp_units <-list(c('Year','GW'))
-r3<- data.frame(db_table_name = "total_mw_offshore_wind",
+offshore_tp_units <- 'gigawatt'
+r3<- data.frame(db_table_name = "total_production_forecast_offshore_wind",
                 full_series_name = 'Total production forecast of offshore wind energy in phases from 2017 through 2035',
                 column2variable_name_map=I(offshore_tp_cols),units=I(offshore_tp_units),frequency='Y',
                 data_source='DEIRP',data_source_full_name='Dominion Energy 2020 Integrated Resource Plan',
