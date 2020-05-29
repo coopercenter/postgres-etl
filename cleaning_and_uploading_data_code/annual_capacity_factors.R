2479339996e8a4c448c742be7f786bd9825ba1ea
Jackson Brandberg <jacksonbrandberg@gmail.com>
2020-05-29 12:42:13 -0400

    edit code so it uploads directly to database

--- cleaning_and_uploading_data_code/annual_capacity_factors.R
+++ cleaning_and_uploading_data_code/annual_capacity_factors.R
@@ -2,17 +2,20 @@ library(dplyr)
 library(tidyverse)
 library(stringr) # for replacing strings
 library(here)
-#here('data','Capacity Factors Annual.csv' )
-#read in dataset
-#capacity.factors.annual <- read.csv(here('data','Capacity Factors Annual.csv' ))
-capacity.factors.annual <- read.csv("~/Downloads/EO43 Data/Capacity Factors Annual.csv")
-capacity.factors.annual <- as.data.frame(t(capacity.factors.annual))
-capacity.factors.annual <- capacity.factors.annual[,2:20]
-names(capacity.factors.annual)<-lapply(capacity.factors.annual[1,],as.character)
-capacity.factors.annual <- capacity.factors.annual[-1,]
-colnames(capacity.factors.annual)[1] <- 'Year'
-capacity.factors.annual[,1] <- str_replace_all(capacity.factors.annual[,1], 'Year', '')
-colnames(retail)<-str_replace_all(colnames(retail),' ','_')
+library("RPostgreSQL")
 
-write.csv(capacity.factors.annual, "Capacity_Factors_Annual_Clean.csv")
+db_driver = dbDriver("PostgreSQL")
+source(here('my_postgres_credentials.R'))
+db <- dbConnect(db_driver,user=db_user, password=ra_pwd,dbname="postgres", host=db_host)
+rm(ra_pwd)
+#read in dataset
+capacity_factors_annual <- read.csv(here('data','Capacity Factors Annual.csv'))
+capacity_factors_annual <- as.data.frame(t(capacity_factors_annual))
+capacity_factors_annual <- capacity_factors_annual[2:20,]
+names(capacity_factors_annual)<-lapply(capacity_factors_annual[1,],as.character)
+capacity_factors_annual <- capacity_factors_annual[-1,]
+capacity_factors_annual <- capacity_factors_annual[-1,]
+colnames(capacity_factors_annual)[1] <- 'Year'
+
+dbWriteTable(db, 'capacity_factors_annual', capacity_factors_annual, row.names=FALSE, overwrite = TRUE)
 
