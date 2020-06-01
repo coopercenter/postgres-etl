f26cefa2d82c6505a1be6133de3ff4c76cbbe1a5
christinazhongg <60795006+christinazhongg@users.noreply.github.com>
2020-06-01 17:49:42 +0800

    wrote a script used to fetch data from the FRED and upload it to the database

--- api_data_code/fetch_from_FRED_api.R
+++ api_data_code/fetch_from_FRED_api.R
@@ -5,92 +5,55 @@ library(tibble)
 library(readr)
 if(!("RPostgreSQL" %in% installed.packages())) install.packages("eia")
 library("RPostgreSQL")
-if(!("eia" %in% installed.packages())) install.packages("eia")
-library(eia)
+
 library(stringr)
 
 # read in my FRED api key
-source(here("etl","my_FRED_api_key.R"))
+source(here("api_data_code","my_fred_api_key.R"))
 
 # function used to fetch the series
 get_FRED_series <- function(api_key,series_id) {
   require(jsonlite)
   require(data.table)
-  # This function retrieves one EIA time-series with metadata
-  # The function returns a list of parts of the series:
-  #     seriesID,name,units,frequency,data (as data table)
-
-  
-  # Example URL
-  # https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=abcdefghijklmnopqrstuvwxyz123456&file_type=json
-  
-  FREDBase = paste0("https://api.stlouisfed.org/fred/series?series_id=",series_id,"&api_key=",api_key,"&file_type=json") 
-  temp = readLines(FREDBase, warn = "F")
+  FREDBase <- paste0("https://api.stlouisfed.org/fred/series/observations?series_id=",series_id,"&api_key=",api_key,"&file_type=json") 
+  temp <- readLines(FREDBase, warn = "F")
   rd <- fromJSON(temp)
-  print(paste0("Retrieving: ",rd$series$series_id))
-  print(paste0(rd$series$name))
-  
+
   # Now take the 'data' element from the list and make a data frame
-  rd2 = data.frame(rd$series$data,stringsAsFactors = F)
-  rd2 = data.table(rd2)
-  
-  setnames(rd2,1,"date"); setnames(rd2,2,'value')
-  rd2[,value:=as.numeric(value)]
-  rd2$date = as.Date(gsub('(\\d{4})(\\d{2})', '\\1-\\2-01', rd2$date))
-  returnList = list(
-    series_id = rd$series$series_id,
-    name = rd$series$name,
-    units = rd$series$units,
-    frequency = rd$series$f,
-    data = rd2
-  )
-  return(returnList) 
+  rd2 <- data.frame(rd$observations,stringsAsFactors = F)
+  rd2 <- data.table(rd2)
+  return(rd2)
 }
-
-data_series = get_FRED_series(my_api_key,"VAPOP")
-
+data_series <- get_FRED_series(fredKey,"VAPOP")
 
 # Connection to the database
 # "my_postgres_login.R" contains the log-in informations of RAs
-source(here("etl", "my_postgres_login.R"))
-drv <- dbDriver("PostgreSQL")
-
-con<- dbConnect(drv, dbname = dbname, host = host, port = port, user = user, password = password)
+source(here("etl", "my_postgres_credentials.R"))
+db_driver = dbDriver("PostgreSQL")
+db <- dbConnect(db_driver,user=db_user, password=ra_pwd,dbname="postgres", host=db_host)
 
 rm(password)
 
-# check the connection
-dbExistsTable(con, "fuel")
-# TRUE
-dbListTables(con)
-
 # Function used to change the names of tables to the format used in MySQL
+## Key: all lower case and no punctuation other than _
 get_name <- function(series_id) {
-  db_table_name <- str_to_lower(paste("eia", str_replace_all(series_id, "[.-]", "_"), sep="_"))
+  db_table_name <- str_to_lower(paste("fred", series_id, sep="_"))
   return(db_table_name)
 }
 
-# apply the function to the list of series id
+# apply the function to the list of series id to get the names for the data tables
 db_table_names <- lapply(series_id_list,get_name)
 
 # Loops through the list of tables and write data series to the PostgreSQL database 
-# "postgres": OVERWRITES EXISTING TABLE!
-# then query the data from postgreSQL
+# then query the data from postgreSQL (query is SQL language meaning writing commands in SQL)
+# "SELECT * FROM datatable_name" means getting the whole table
+# Note: This code OVERWRITES EXISTING TABLE!
+
 for (i in 1:length(all_data_series)){
-  dbWriteTable(con, db_table_names[[i]], value = all_tables[[i]], append = FALSE, overwrite = TRUE, row.names = FALSE)
-  dbGetQuery(con, paste("SELECT * from",db_table_names[[i]])) %>% as_tibble() -> df_postgres
+  dbWriteTable(db, db_table_names[[i]], value = all_tables[[i]], append = FALSE, overwrite = TRUE, row.names = FALSE)
+  dbGetQuery(db, paste("SELECT * from",db_table_names[[i]])) %>% as_tibble() -> df_postgres
 }
 
-fuel<-dbGetQuery(con,'SELECT * from fuel')
-postgresqlDescribeResult(fuel)
-
-
-
-
-
 # Close connection
-dbDisconnect(con)
-dbUnloadDriver(drv)
-
-
-
+dbDisconnect(db)
+dbUnloadDriver(db_driver)
---
+++ api_data_code/my_fred_api_key.R
@@ -0,0 +1,1 @@
+fredKey <- '8e8c1e7aa3720dfebe504d686b97d1ac'
