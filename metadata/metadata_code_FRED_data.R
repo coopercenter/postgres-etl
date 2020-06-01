f258f5b38928d4d4a42a7285c959afec28f06465
christinazhongg <60795006+christinazhongg@users.noreply.github.com>
2020-06-01 18:47:05 +0800

    wrote a script to uploade the metadata for FRED data

--- metadata/metadata_code_FRED_data.R
+++ metadata/metadata_code_FRED_data.R
@@ -17,7 +17,7 @@ metadata <- dbGetQuery(db,'SELECT * from metadata')
 # ----------------------------------------------------------------------------------
 res_pop<-dbGetQuery(db,'SELECT * FROM residential_population_va')
 
-res_pop <- colnames(res_pop)
+res_col <- list(c('realtime_start','realtime_end','date','value'))
 
 get_FRED_meta <- function(api_key,series_id) {
   require(jsonlite)
@@ -31,7 +31,7 @@ get_FRED_meta <- function(api_key,series_id) {
     series_id = rd$seriess$id,
     name = rd$seriess$title,
     units = rd$seriess$units,
-    frequency = rd$seriess$frequency_short,
+    frequency = rd$seriess$frequency_short
   )
   return(returnList) 
 }
@@ -39,47 +39,13 @@ get_FRED_meta <- function(api_key,series_id) {
 source(here("api_data_code","my_fred_api_key.R"))
 pop_meta <- get_FRED_meta(fredKey,"VAPOP")
 
-offshore_mw_units <-'megawatts'
-
-r1<- data.frame(db_table_name = "total_mw_offshore_wind",
-                short_series_name= 'Total megawatt predictions of offshore wind',
-                full_series_name = 'Total megawatt predictions of offshore wind energy in phases from 2017 through 2033',
-                column2variable_name_map=I(offshore_mw_cols),units=I(offshore_mw_units),frequency='A',
-                data_source_brief_name='DEIRP',data_source_full_name='Dominion Energy 2020 Integrated Resource Plan',
-                url='https://www.dominionenergy.com/library/domcom/media/about-us/making-energy/2020-va-integrated-resource-plan.pdf?modified=20200501191108',api=NA,
-                series_id=NA,json=NA,notes=NA)
-
-# ----------------------------------------------------------------------------------
-net_capacity_factor_offshore_wind<-dbGetQuery(db,'SELECT * from net_capacity_factor_offshore_wind')
-
-colnames(net_capacity_factor_offshore_wind)
-offshore_cf_cols <- list(c('Year','Pilot','Stage_I','Stage_II',
-                           'Stage_III'))
-offshore_cf_units <- 'CF'
-
-r2<- data.frame(db_table_name = "net_capacity_factor_offshore_wind",
-                short_series_name='Capacity factor predictions of offshore wind',
-                full_series_name = 'Capacity factor predictions of offshore wind energy in phases from 2017 through 2035',
-                column2variable_name_map=I(offshore_cf_cols),units=I(offshore_cf_units),frequency='A',
-                data_source_brief_name='DEIRP',data_source_full_name='Dominion Energy 2020 Integrated Resource Plan',
-                url='https://www.dominionenergy.com/library/domcom/media/about-us/making-energy/2020-va-integrated-resource-plan.pdf?modified=20200501191108',api=NA,
-                series_id=NA,json=NA,notes=NA)
-
-# ----------------------------------------------------------------------------------
-total_production_forecast_offshore_wind<-dbGetQuery(db,'SELECT * from total_production_forecast_offshore_wind')
-colnames(total_production_forecast_offshore_wind)
-offshore_tp_cols <- list(c('Year','Total_Production_GWh'))
-offshore_tp_units <- 'gigawatt'
-r3<- data.frame(db_table_name = "total_production_forecast_offshore_wind",
-                short_series_name = 'Total production forecast of offshore wind',
-                full_series_name = 'Total production forecast of offshore wind energy in phases from 2017 through 2035',
-                column2variable_name_map=I(offshore_tp_cols),units=I(offshore_tp_units),frequency='Y',
-                data_source_brief_name='DEIRP',data_source_full_name='Dominion Energy 2020 Integrated Resource Plan',
-                url='https://www.dominionenergy.com/library/domcom/media/about-us/making-energy/2020-va-integrated-resource-plan.pdf?modified=20200501191108',api=NA,
-                series_id=NA,json=NA,notes=NA)
-
-library(plyr)
-metadata<-rbind(r1,r2,r3)
-dbWriteTable(db, 'metadata', value = metadata, append = TRUE, overwrite = FALSE, row.names = FALSE)
+r1<- data.frame(db_table_name = "residential_population_va",
+                short_series_name= 'VA residential population',
+                full_series_name = 'Virginia residential population',
+                column2variable_name_map=I(res_col),units=I(pop_meta$units),frequency=pop_meta$frequency,
+                data_source_brief_name='FRED',data_source_full_name='Federal Reserve Economic Data',
+                url=NA,api='https://fred.stlouisfed.org/series/VAPOP',
+                series_id='VAPOP',json=NA,notes=NA)
 
 
+dbWriteTable(db, 'metadata', value = r1, append = TRUE, overwrite = FALSE, row.names = FALSE)
