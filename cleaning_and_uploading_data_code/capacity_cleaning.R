18b76e44c0616addd94d8346046562a3ab87225c
christinazhongg <60795006+christinazhongg@users.noreply.github.com>
2020-06-02 16:33:17 +0800

    delete redundant file

--- cleaning_and_uploading_data_code/capacity_cleaning.R
+++
@@ -1,9 +0,0 @@
-capacity <- read.csv(here('raw_data', '2A_Capacity.csv'))
-library(dplyr)
-capacity <- capacity[2:12,2:5]
-names(capacity)<-lapply(capacity[1,],as.character)
-capacity <- capacity[-1,]
-colnames(capacity)<-str_replace_all(colnames(capacity),' ','_')
-
-#write.csv(capacity,file = 'capacity_2A.csv') -- remove statement
-#upload to db!
