f1ed8fd9913b9a7c7cbd9d66e21fee1bfad31db4
LEEPYUNG <65184551+LEEPYUNG@users.noreply.github.com>
2020-06-09 12:25:43 -0400

    Fixed formatting and naming issues.

--- cleaning_and_uploading_data_code/cleaning_VCEA_goals.R
+++ cleaning_and_uploading_data_code/cleaning_VCEA_goals.R
@@ -14,23 +14,23 @@ VCEA <- read_excel(here('raw_data','VCEA_goals.xlsx'))
 VCEA <- as.data.frame(VCEA)
 
 #Creating apco_dominion_rps dataset 
-renewable_portfolio_standards <- select(VCEA, c(1:3))
+VCEA_renewable_portfolio_standards <- select(VCEA, c(1:3))
 
 #Creating apco_dominion_onshore_wind_solar
-apco_dominion_onshore_wind_solar <- select(VCEA, c(1, 4:5))
+VCEA_onshore_wind_solar <- select(VCEA, c(1,4:5))
 
 #Creating apco_dominion_storage
-apco_dominion_storage <- select(VCEA, c(1, 6:7))
+VCEA_storage <- select(VCEA, c(1,6:7))
 
 #Creating apco_dominion_energy_effciency
-apco_dominion_energy_effciency <- select(VCEA, c(1, 8:9))
+VCEA_energy_effciency <- select(VCEA, c(1,8:9))
 
 
 #upload to db
-dbWriteTable(db, 'renewable_portfolio_standards', renewable_portfolio_standards, row.names=FALSE, overwrite = TRUE)
-dbWriteTable(db, 'apco_dominion_onshore_wind_solar', apco_dominion_onshore_wind_solar, row.names=FALSE, overwrite = TRUE)
-dbWriteTable(db, 'apco_dominion_storage', apco_dominion_storage, row.names=FALSE, overwrite = TRUE)
-dbWriteTable(db, 'apco_dominion_energy_effciency', apco_dominion_energy_effciency, row.names=FALSE, overwrite = TRUE)
+dbWriteTable(db, 'VCEA_renewable_portfolio_standards', VCEA_renewable_portfolio_standards, row.names=FALSE, overwrite = TRUE)
+dbWriteTable(db, 'VCEA_onshore_wind_solar', VCEA_onshore_wind_solar, row.names=FALSE, overwrite = TRUE)
+dbWriteTable(db, 'VCEA_storage',VCEA_storage, row.names=FALSE, overwrite = TRUE)
+dbWriteTable(db, 'VCEA_energy_effciency', VCEA_energy_effciency, row.names=FALSE, overwrite = TRUE)
 
 #close db connection
 dbDisconnect(db)
