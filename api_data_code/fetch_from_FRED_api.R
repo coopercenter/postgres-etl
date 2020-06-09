7391408f5de38f0615aa47102e2cb194153e08da
LEEPYUNG <65184551+LEEPYUNG@users.noreply.github.com>
2020-06-09 14:14:52 -0400

    Edited fetch_from_FRED so that it only pulls VAPOP and VANGSP

--- api_data_code/fetch_from_FRED_api.R
+++ api_data_code/fetch_from_FRED_api.R
@@ -18,32 +18,23 @@ get_FRED_series <- function(api_key,series_id) {
   FREDBase <- paste0("https://api.stlouisfed.org/fred/series/observations?series_id=",series_id,"&api_key=",api_key,"&file_type=json") 
   temp <- readLines(FREDBase, warn = "F")
   rd <- fromJSON(temp)
-
+  
   # Now take the 'data' element from the list and make a data frame
   rd2 <- data.frame(rd$observations,stringsAsFactors = F)
   rd2 <- data.table(rd2)
   return(rd2)
 }
 
-# Make a list of series ids
-# If you have a large number of datasets, create a txt file storing the series ids
-## reads in a txt file containing the series ids of the datasets we need. Makse sure to use series ID for FRED NOT for EIA.
-series_id_vec <- read_file(here("api_data_code","series_ids2.txt"))
-
-## transform the content to a list that we can later feed into the fetch function
-series_id_list <- unlist(strsplit(series_id_vec,'\r\n'))
-
-## create an empty list to store the data tables
-all_tables<- vector("list", length(series_id_list))
-
-# loops through the all data series and store the tables in to a big list
-for (i in 1:length(all_tables)){
-  all_tables[[i]]<-get_FRED_series(fredKey,series_id_list[[i]])
-}
+#Creates a list of data tables 
+data_series <- vector("list", 2)
+data_series[[1]] <- get_FRED_series(fredKey,"VAPOP")
+#Clean up data, gets rid of first two columns.
+data_series_VANGSP <- select(get_FRED_series(fredKey,"VANGSP"),c(3:4))
+data_series[[2]] <- data_series_VANGSP
 
 # Connection to the database
 # "my_postgres_login.R" contains the log-in informations of RAs
-source(here("etl", "my_postgres_credentials.R"))
+source(here("api_data_code","my_postgres_credentials.R"))
 db_driver = dbDriver("PostgreSQL")
 db <- dbConnect(db_driver,user=db_user, password=ra_pwd,dbname="postgres", host=db_host)
 
@@ -57,20 +48,13 @@ get_name <- function(series_id) {
 }
 
 # apply the function to the list of series id to get the names for the data tables
-## create an empty list to store thenames for the data tables
-db_table_names <- vector("list", length(series_id_list))
-db_table_names <- lapply(series_id_list,get_name)
+db_table_names <- vector("list", 2)
+db_table_names[[1]] <- get_name("VAPOP")
+db_table_names[[2]] <- get_name("VANGSP")
 
-# Loops through the list of tables and write data series to the PostgreSQL database 
-# then query the data from postgreSQL (query is SQL language meaning writing commands in SQL)
-# "SELECT * FROM datatable_name" means getting the whole table
-# Note: This code OVERWRITES EXISTING TABLE!
+#Upload data into database.
+dbWriteTable(db, db_table_names[[1]], value = data_series[[1]], append = FALSE, overwrite = TRUE, row.names = FALSE)
+dbWriteTable(db, db_table_names[[2]], value = data_series[[2]], append = FALSE, overwrite = TRUE, row.names = FALSE)
 
-for (i in 1:length(all_data_series)){
-  dbWriteTable(db, db_table_names[[i]], value = all_tables[[i]], append = FALSE, overwrite = TRUE, row.names = FALSE)
-  dbGetQuery(db, paste("SELECT * from",db_table_names[[i]])) %>% as_tibble() -> df_postgres
-}
-
-# Close connection
 dbDisconnect(db)
 dbUnloadDriver(db_driver)
