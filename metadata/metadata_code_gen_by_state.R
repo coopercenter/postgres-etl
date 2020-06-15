766243ec6e9de2333d30b33da8cf4bbf633ef83c
Chloe Fauvel <cff2aa@virginia.edu>
2020-06-15 17:17:16 -0400

    edited metadata to include data refresh columns

--- metadata/metadata_code_gen_by_state.R
+++ metadata/metadata_code_gen_by_state.R
@@ -1,12 +1,14 @@
 library(here)
+library(eia)
 library('RPostgreSQL')
 library(tidyverse)
 source(here("api_data_code","my_eia_api_key.R"))
 source(here("my_postgres_credentials.R"))
+
+eia_set_key(eiaKey)
+
 db_driver <- dbDriver("PostgreSQL")
-
 db <- dbConnect(db_driver,user=db_user, password=ra_pwd,dbname="postgres", host=db_host)
-
 rm(ra_pwd)
 
 # check the connection
@@ -31,7 +33,7 @@ for(state in states){
   gen_by_source_cols <- list(c('year','coal','oil','gas','nuclear',
                                'wind','utility_solar','distributed_solar',
                                'hydropower','wood','other_biomass','total'))
-  gen_by_source_units <-'gigawatthours'
+  gen_by_source_units <-'GWh'
   
   series_ids=list(c(paste0("ELEC.GEN.COW-",state,"-99.A"),
               paste0("ELEC.GEN.PEL-",state,"-99.A"),
@@ -47,11 +49,15 @@ for(state in states){
   )
   
   api_link <- vector("list", length(series_ids))
-  
   for (i in 1:length(series_ids)){
     api_link[[i]] <- paste("http://api.eia.gov/series/?api_key=",eiaKey,"&series_id=",series_ids[[i]],sep='')
   }
   
+  data_update <- eia_series_updates(paste0("ELEC.GEN.ALL-",state,"-99.A"))$updated #only timestamping series for total generation per state
+  data_update <- gsub("T", " ", data_update)
+  data_update <- gsub("-0400", "", data_update)
+  data_update <- strptime(data_update, format="%Y-%m-%d %H:%M:%S")
+
   
   r1_year <- data.frame(db_table_name = paste0("eia_elec_gen_",str_to_lower(state),"_a",sep =""),
                   short_series_name= paste('Total annual generation for ',state,sep=''),
@@ -60,7 +66,8 @@ for(state in states){
                   data_source_brief_name='EIA',data_source_full_name='U.S. Energy Information Administration',
                   url=NA, api=I(api_link),
                   series_id=I(series_ids),json=NA,notes=NA, mandate=0, forecast=0, corresponding_data=NA, 
-                  R_script='generation_all_states.R')
+                  R_script='generation_all_states.R', latest_data_update=data_update,
+                  last_db_refresh=lubridate::with_tz(Sys.time(), "UTC"))
   
   if (is.null(all_r1_year))
   {all_r1_year <- r1_year}
@@ -74,7 +81,7 @@ for(state in states){
   gen_by_source_cols <- list(c('year','coal','oil','gas','nuclear',
                                'wind','utility_solar','distributed_solar',
                                'hydropower','wood','other_biomass','total'))
-  gen_by_source_units <-'gigawatthours'
+  gen_by_source_units <-'GWh'
   
   series_ids=list(c(paste0("ELEC.GEN.COW-",state,"-99.M"),
                     paste0("ELEC.GEN.PEL-",state,"-99.M"),
@@ -95,6 +102,12 @@ for(state in states){
     api_link[[i]] <- paste("http://api.eia.gov/series/?api_key=",eiaKey,"&series_id=",series_ids[[i]],sep='')
   }
   
+  data_update <- eia_series_updates(paste0("ELEC.GEN.ALL-",state,"-99.M"))$updated #only timestamping series for total generation per state
+  data_update <- gsub("T", " ", data_update)
+  data_update <- gsub("-0400", "", data_update)
+  data_update <- strptime(data_update, format="%Y-%m-%d %H:%M:%S")
+  
+  
   r1_month<- data.frame(db_table_name = paste0("eia_elec_gen_",str_to_lower(state),"_m",sep =""),
                   short_series_name= paste('Total monthly generation for ',state, sep=''),
                   full_series_name = paste('Net generation : by fuel : ',state,' : monthly',sep=''),
@@ -102,7 +115,8 @@ for(state in states){
                   data_source_brief_name='EIA',data_source_full_name='U.S. Energy Information Administration',
                   url=NA, api=I(api_link),
                   series_id=I(series_ids),json=NA,notes=NA, mandate=0, forecast=0, corresponding_data=NA, 
-                  R_script='generation_all_states.R')
+                  R_script='generation_all_states.R', latest_data_update=data_update,
+                  last_db_refresh=lubridate::with_tz(Sys.time(), "UTC"))
   
   if (is.null(all_r1_month))
   {all_r1_month <- r1_month}
@@ -112,6 +126,6 @@ for(state in states){
 }
 
 r1 <- rbind(all_r1_year,all_r1_month)
-dbWriteTable(db, 'metadata', value = r1, append = TRUE, overwrite = FALSE, row.names = FALSE)
-
+#dbWriteTable(db, 'metadata', value = r1, append = TRUE, overwrite = FALSE, row.names = FALSE)
+dbWriteTable(db, 'test', value = r1, append = TRUE, overwrite = FALSE, row.names = FALSE)
 
