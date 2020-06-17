29cc00d1f29ebbd1964ccbb6f72160d1618c4539
christinazhongg <60795006+christinazhongg@users.noreply.github.com>
2020-06-17 22:06:11 +0800

    back to thousands

--- deriving_and_uploading_data_code/deriving_carbon_dioxide_emissions_per_GDP.R
+++ deriving_and_uploading_data_code/deriving_carbon_dioxide_emissions_per_GDP.R
@@ -21,11 +21,11 @@ tot_emission <- dbGetQuery(db,'SELECT * from eia_emiss_co2_totv_tt_to_va_a')
 tot_s60<- tot_emission[21:1,1]*1000000 #1997 to 2017
 
 # derive the values
-e_per_gdp <- tot_s60/gdp_s60
+e_per_gdp <- (tot_s60/gdp_s60)*1000
 e_per_gdp_df <- data.frame(1997:2017,e_per_gdp)
-colnames(e_per_gdp_df)<- c('year','co2_emission_per_unit_of_GDP')
+colnames(e_per_gdp_df)<- c('year','co2_emission_per_thousand_dollars_of_gdp')
 
 #upload to db
-dbWriteTable(db, 'carbon_dioxide_emissions_per_gdp_va', e_per_gdp_df, row.names=FALSE, overwrite = TRUE)
+dbWriteTable(db, 'co2_emission_per_thousand_dollars_of_gdp_va', e_per_gdp_df, row.names=FALSE, overwrite = TRUE)
 #close db connection
 dbDisconnect(db)
