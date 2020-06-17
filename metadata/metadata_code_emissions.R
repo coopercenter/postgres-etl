b27fad99523592a8a1661e961c654a2f0f3690da
Chloe Fauvel <cff2aa@virginia.edu>
2020-06-17 13:47:38 -0400

    metadata edits

--- metadata/metadata_code_emissions.R
+++ metadata/metadata_code_emissions.R
@@ -17,53 +17,47 @@ metadata <- dbGetQuery(db,'SELECT * from metadata')
 
 # ----------------------------------------------------------------------------------
 emissions_co2_by_source_va<-dbGetQuery(db,'SELECT * from emissions_co2_by_source_va')
-
-colnames(emissions_co2_by_source_va)
-co2_by_source_cols <- list(c('Year','Coal','Natural_gas','other','petroleum',
-                             'total'))
-co2_by_source_units <-'megawatthour'
+co2_by_source_cols <- list(colnames(emissions_co2_by_source_va))
+co2_by_source_units <-'MWh'
 
 r1<- data.frame(db_table_name = "emissions_co2_by_source_va",
-                short_series_name= 'Total megawatt hour of co2 emissions',
-                full_series_name = 'Total megawatt hour of co2 emissions in phases from 1990 through 2018',
+                short_series_name= 'Total megawatt hour of carbon dioxide emissions',
+                full_series_name = 'Total megawatt hour of carbon dioxide emissions in phases from 1990 through 2018',
                 column2variable_name_map=I(co2_by_source_cols ),units=I(co2_by_source_units),frequency='A',
                 data_source_brief_name='EIA',data_source_full_name='U.S. Energy Information Administration',
                 url='https://www.eia.gov/electricity/state/virginia/',api=NA,
-                series_id=NA,json=NA,notes=NA, mandate=0, forecast=0, corresponding_data=NA, 
+                series_id=NA,json=NA,notes=NA, data_type='time-series', data_context='historical', corresponding_data=NA, 
                 R_script='cleaning_emissions.R')
 
 # ----------------------------------------------------------------------------------
 emissions_no_by_source_va<-dbGetQuery(db,'SELECT * from emissions_no_by_source_va')
-
-colnames(emissions_no_by_source_va)
-no_by_source_cols <- list(c('Year','Coal','Natural_gas','other','petroleum',
-                            'total'))
-no_by_source_units <- 'megawatt hour'
+no_by_source_cols <- list(colnames(emissions_no_by_source_va))
+no_by_source_units <- 'MWh'
 
 r2<- data.frame(db_table_name = "emissions_no_by_source_va",
-                short_series_name='Total megawatt hour of no emissions',
-                full_series_name = 'Total megawatt hour of no emissions in phases from 1990 through 2018',
+                short_series_name='Total megawatt hour of nitrogen oxide emissions',
+                full_series_name = 'Total megawatt hour of nitrogen oxide emissions in phases from 1990 through 2018',
                 column2variable_name_map=I(no_by_source_cols),units=I(no_by_source_units),frequency='A',
                 data_source_brief_name='EIA',data_source_full_name='U.S. Energy Information Administration',
                 url='https://www.eia.gov/electricity/state/virginia/',api=NA,
-                series_id=NA,json=NA,notes=NA, mandate=0, forecast=0, corresponding_data=NA, 
+                series_id=NA,json=NA,notes=NA, data_type='time-series', data_context='historical', corresponding_data=NA, 
                 R_script='cleaning_emissions.R')
 
 # ----------------------------------------------------------------------------------
 emissions_so2_by_source_va<-dbGetQuery(db,'SELECT * from emissions_so2_by_source_va')
-colnames(emissions_so2_by_source_va)
-so2_by_source_cols<- list(c('Year','Coal','Natural_gas','other','petroleum',
-                            'total'))
-so2_by_source_units <- 'megawatt hour'
+so2_by_source_cols<- list(colnames(emissions_so2_by_source_va))
+so2_by_source_units <- 'MWh'
+
 r3<- data.frame(db_table_name = "emissions_so2_by_source_va",
-                short_series_name = 'Total megawatt hour of so2 emissions',
-                full_series_name = 'Total megawatt hour of no emissions in phases from 1990 through 2018',
+                short_series_name = 'Total megawatt hour of sulfur dioxide emissions',
+                full_series_name = 'Total megawatt hour of sulfur dioxide emissions in phases from 1990 through 2018',
                 column2variable_name_map=I(so2_by_source_cols),units=I(so2_by_source_units),frequency='A',
                 data_source_brief_name='EIA',data_source_full_name='U.S. Energy Information Administration',
                 url='https://www.eia.gov/electricity/state/virginia/',api=NA,
-                series_id=NA,json=NA,notes=NA, mandate=0, forecast=0, corresponding_data=NA, 
+                series_id=NA,json=NA,notes=NA, data_type='time-series', data_context='historical', corresponding_data=NA, 
                 R_script='cleaning_emissions.R')
 
+# ----------------------------------------------------------------------------------
 library(plyr)
 metadata<-rbind(r1,r2,r3)
 dbWriteTable(db, 'metadata', value = metadata, append = TRUE, overwrite = FALSE, row.names = FALSE)
--- metadata/metadata_code_energy_burden.R
+++ metadata/metadata_code_energy_burden.R
@@ -6,6 +6,7 @@ library(here)
 library('RPostgreSQL')
 library(tidyverse)
 source(here('my_postgres_credentials.R'))
+
 db_driver <- dbDriver("PostgreSQL")
 db <- dbConnect(db_driver,user=db_user, password=ra_pwd,dbname="postgres", host=db_host)
 rm(ra_pwd)
@@ -14,6 +15,7 @@ rm(ra_pwd)
 # if this returns true, it means that you are connected to the database now
 dbExistsTable(db, "metadata")
 
+# ----------------------------------------------------------------------------------
 # get the cleaned dataset from the database
 ## fuel_cleaned should be replaced by the name of your dataset in the database
 ## (coordinate with Jackson)
@@ -28,41 +30,45 @@ energy_burden_county_percent_income_cols<-list(colnames(energy_burden_county_per
 
 # put the unit of each column into a list
 energy_burden_by_fuel_type_units <-list(c('fuel_type','percent','dollar','dollar','dollar','dollar','percent'))
-energy_burden_county_expenditures_units <-list(c('county', 'avg_annual_energy_cost'))
-energy_burden_county_percent_income_units <-list(c('county', 'avg_energy_burden_as_percent_income'))
+energy_burden_county_expenditures_units <- 'dollar'
+energy_burden_county_percent_income_units <- 'percent'
 
+# ----------------------------------------------------------------------------------
 # Construct a data frame with only one row, if you have more than one dataset,
 # construct r2, r3,... if needed
 r1<- data.frame(db_table_name = "energy_burden_by_fuel_type", 
                 short_series_name = "Energy burden by fuel type",
                 full_series_name = 'Energy cost annually by fuel type',
-                column2variable_name_map=I(energy_burden_by_fuel_type_cols),units=I(energy_burden_by_fuel_type_units),frequency='O',
+                column2variable_name_map=I(energy_burden_by_fuel_type_cols),units=I(energy_burden_by_fuel_type_units),frequency=NA,
                 data_source_brief_name='LEAD',data_source_full_name='Low-Income Energy Affordability Data Tool',
                 url='https://www.energy.gov/eere/slsc/maps/lead-tool',api=NA,
-                series_id=NA,json=NA,notes=NA, mandate=0, forecast=0, corresponding_data=NA, 
+                series_id=NA,json=NA,notes=NA, data_type='cross-sectional', data_context='historical', 
+                corresponding_data=NA, 
                 R_script='cleaning_energy_burden.R')
 
 r2<- data.frame(db_table_name = "energy_burden_county_expenditures",
                 short_series_name = "Energy burden by county",
                 full_series_name = 'Energy cost annually per county',
-                column2variable_name_map=I(energy_burden_county_expenditures_cols),units=I(energy_burden_county_expenditures_units),frequency='O',
+                column2variable_name_map=I(energy_burden_county_expenditures_cols),units=I(energy_burden_county_expenditures_units),frequency=NA,
                 data_source_brief_name='LEAD',data_source_full_name='Low-Income Energy Affordability Data Tool',
                 url='https://www.energy.gov/eere/slsc/maps/lead-tool',api=NA,
-                series_id=NA,json=NA,notes=NA, mandate=0, forecast=0, corresponding_data=NA, 
+                series_id=NA,json=NA,notes=NA, data_type='cross-sectional', data_context='historical', 
+                corresponding_data=NA, 
                 R_script='cleaning_energy_burden.R')
 
 r3<- data.frame(db_table_name = "energy_burden_county_percent_income",
                 short_series_name = "Energy burden per percent income",
                 full_series_name = 'Energy cost annually per percent income',
-                column2variable_name_map=I(energy_burden_county_percent_income_cols),units=I(energy_burden_county_percent_income_units),frequency='O',
+                column2variable_name_map=I(energy_burden_county_percent_income_cols),units=I(energy_burden_county_percent_income_units),frequency=NA,
                 data_source_brief_name='LEAD',data_source_full_name='Low-Income Energy Affordability Data Tool',
                 url='https://www.energy.gov/eere/slsc/maps/lead-tool',api=NA,
-                series_id=NA,json=NA,notes=NA, mandate=0, forecast=0, corresponding_data=NA, 
+                series_id=NA,json=NA,notes=NA, data_type='cross-sectional', data_context='historical', 
+                corresponding_data=NA, 
                 R_script='cleaning_energy_burden.R')
 
+# ----------------------------------------------------------------------------------
 library(plyr)
 metadata <- rbind(r1, r2, r3)
-
 
 # Append your rows to the metadata table in our database
 dbWriteTable(db, 'metadata', value = metadata, append = TRUE, overwrite = FALSE, row.names = FALSE)
