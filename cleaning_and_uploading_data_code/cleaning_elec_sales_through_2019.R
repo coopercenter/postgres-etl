98d25f04a5b5c9d63f0b4eb7283b6da947d4b1f1
LEEPYUNG <65184551+LEEPYUNG@users.noreply.github.com>
2020-07-07 10:26:15 -0400

    Uploading elec sales historic and forecasted data

---
+++ cleaning_and_uploading_data_code/cleaning_elec_sales_through_2019.R
@@ -0,0 +1,44 @@
+library(dplyr)
+library(tidyverse)
+library(stringr) # for replacing strings
+library(here)
+library("RPostgreSQL")
+library(readxl)
+
+db_driver = dbDriver("PostgreSQL")
+source(here('my_postgres_credentials.R'))
+db <- dbConnect(db_driver,user=db_user, password=ra_pwd,dbname="postgres", host=db_host)
+rm(ra_pwd)
+
+#read in dataset
+elec_sales_through_2019 <- read_excel(here('raw_data','elec_sales_forecasts_2019-12.xlsx'),sheet = "Annual",col_names = TRUE)
+elec_sales_through_2019 <- select(elec_sales_through_2019, c(1,3,5,15,17))
+
+#replacing row names 
+colnames(elec_sales_through_2019)<- c("Year", "APCO GWh","Dominion GWh", "APCO GWh Forecast","Dominion Gwh Forecast")
+
+#upload to db
+dbWriteTable(db, 'elec_sales_through_2019', elec_sales_through_2019, row.names=FALSE, overwrite = TRUE)
+
+#close db connection
+dbDisconnect(db)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
