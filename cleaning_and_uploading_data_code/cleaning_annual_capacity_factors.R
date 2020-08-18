9889c91a98d7a6e2acfceda5694a1db10c66bfe0
Pyung Lee <pyungklee@gmail.com>
2020-08-18 15:03:14 -0400

    fixed cleaning_annuall_capacity script.
    Edited so the years are just numeric.

--- cleaning_and_uploading_data_code/cleaning_annual_capacity_factors.R
+++ cleaning_and_uploading_data_code/cleaning_annual_capacity_factors.R
@@ -18,6 +18,15 @@ names(capacity_factors_annual)<-lapply(capacity_factors_annual[1,],as.character)
 capacity_factors_annual <- capacity_factors_annual[-1,]
 colnames(capacity_factors_annual)[1] <- 'Year'
 
+years <- c(2018, 2017,2016,2015,2014,2013,2012,2011,2010, 2009, 2008)
+index <- c(2:12)
+i <- c(1:11)
+
+for (val in i){
+  capacity_factors_annual[index[i],1] = years[i]
+}
+
+
 dbWriteTable(db, 'capacity_factors_annual', capacity_factors_annual, row.names=FALSE, overwrite = TRUE)
 #close db connection
 dbDisconnect(db)
