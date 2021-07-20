3551438d123c80bbf2385d0beade7343cd8e15d3
caneale320 <caneale320@gmail.com>
2021-07-20 10:30:06 -0400

    fix hard coded indicies

--- deriving_and_uploading_data_code/deriving_co2_emissions.R
+++ deriving_and_uploading_data_code/deriving_co2_emissions.R
@@ -15,36 +15,41 @@ dbExistsTable(db, "metadata")
 #-------------------------------------------------------------------------------------------------------
 # get the datasets of gdp from 1997 to 2017
 gdp <- dbGetQuery(db,'SELECT * from fred_vangsp')
+
 # unit of gdp is millions of dollars
-gdp_s97<- as.numeric(gdp[1:21,2])*1000000 #Now this information will show total dollars. 1997 to 2017
+gdp$value<- as.numeric(gdp$value)*1000 
+gdp$date <- as.Date(gdp$date)
+#Now this information will show total dollars. 1997 to 2017
+
 tot_emission <- dbGetQuery(db,'SELECT * from eia_emiss_co2_totv_tt_to_va_a') 
 # original unit of emission is million metric tons CO2
-tot_s97<- tot_emission[21:1,1]*1000000 #1997 to 2017
+tot_emission$value<- tot_emission$value*1000000
+
+# join data
+merged <- full_join(gdp, tot_emission, by="date", suffix=c("_gdp", "_emission"))
 
 # derive the values
-e_per_gdp <- (tot_s97/gdp_s97)*1000
-e_per_gdp_df <- data.frame(1997:2017,e_per_gdp)
-colnames(e_per_gdp_df)<- c('year','co2_emission_per_thousand_dollars_of_gdp')
+merged$co2_emission_per_thousand_dollars_of_gdp <- (merged$value_emission/merged$value_gdp)
+e_per_gdp_df <- merged %>% select(year, co2_emission_per_thousand_dollars_of_gdp) %>% filter(!is.na(co2_emission_per_thousand_dollars_of_gdp))
 
 #upload to db
 dbWriteTable(db, 'co2_emission_per_thousand_dollars_of_gdp_va', e_per_gdp_df, row.names=FALSE, overwrite = TRUE)
 #-------------------------------------------------------------------------------------------------------
 # get the datasets of population from 1980 to 2017
 pop <- dbGetQuery(db,'SELECT * from fred_vapop')
+
 # unit of population is thousands of persons
-pop_s80<- as.numeric(pop[81:118,4])*1000 
-#Now this information will show total persons.
-tot_emission <- dbGetQuery(db,'SELECT * from eia_emiss_co2_totv_tt_to_va_a') 
-# original unit of emission is million metric tons CO2
-tot_s80<- tot_emission[nrow(tot_emission):1,1]*1000000 #1980 to 2017
-# now it's metric tons
+pop$value <- as.numeric(pop$value)*1000 
+pop$date <- as.Date(pop$date)
+
+merged <- full_join(pop, tot_emission, by="date", suffix=c("_pop", "_emission"))
 
 # derive the values
-e_per_cap <- tot_s80/pop_s80
-e_per_cap_df <- data.frame(1980:2017,e_per_cap)
-colnames(e_per_cap_df)<- c('year','co2_emission_per_capita')
+merged$co2_emission_per_capita <- merged$value_emission/merged$value_pop
 
-#upload to db
+e_per_cap_df <- merged %>% select(year, co2_emission_per_capita) %>% filter(!is.na(co2_emission_per_capita))
+
+# upload to db
 dbWriteTable(db, 'co2_emission_per_capita_va', e_per_cap_df, row.names=FALSE, overwrite = TRUE)
 
 #-------------------------------------------------------------------------------------------------------
